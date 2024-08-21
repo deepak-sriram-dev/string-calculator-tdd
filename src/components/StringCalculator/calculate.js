e1e5ae0af7407608108728332dcdb148119686c9
deepak-sriram <deepaksriramdev@gmail.com>
2024-08-21 17:51:49 +0530

    New: add() for calculating the string integer values

---
+++ src/components/StringCalculator/calculate.js
@@ -0,0 +1,29 @@
+export function add(numbers) {
+  if (numbers === "") return 0;
+  const numberArray = parseNumbers(numbers);
+  validateNumbers(numberArray);
+  return sumNumbers(numberArray);
+}
+
+function parseNumbers(numbers) {
+  if (numbers.startsWith("//")) {
+    const delimiter = numbers.substring(2, numbers.indexOf("\n"));
+    numbers = numbers.substring(numbers.indexOf("\n") + 1);
+    numbers = numbers.replace(new RegExp(`\\${delimiter}`, "g"), ",");
+  }
+
+  numbers = numbers.replace("\n", ",");
+
+  return numbers.split(",");
+}
+
+function validateNumbers(numbers) {
+  const negativeNumbers = numbers.filter((num) => parseInt(num) < 0);
+  if (negativeNumbers.length > 0) {
+    throw new Error("Negatives not allowed: " + negativeNumbers.join(", "));
+  }
+}
+
+function sumNumbers(numbers) {
+  return numbers.reduce((sum, num) => sum + parseInt(num), 0);
+}
