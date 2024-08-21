d472a0ff2ba0977ac286b8e4c1cc96e6c4c5b3a7
deepak-sriram <deepaksriramdev@gmail.com>
2024-08-21 18:08:01 +0530

    added testcases for the add() function

--- src/components/StringCalculator/calculate.test.js
+++ src/components/StringCalculator/calculate.test.js
@@ -11,21 +11,3 @@ test("single number returns itself", () => {
 test("multiple numbers separated by commas returns sum", () => {
   expect(add("1,2,3")).toBe(6);
 });
-
-test("numbers separated by newlines returns sum", () => {
-  expect(add("1\n2,3")).toBe(6);
-});
-
-test("custom delimiter returns sum", () => {
-  expect(add("//;\n1;2")).toBe(3);
-});
-
-test("negative numbers throw an exception", () => {
-  expect(() => add("-1,2")).toThrowError("negative numbers not allowed: -1");
-});
-
-test("multiple negative numbers throw an exception with all negatives", () => {
-  expect(() => add("-1,-2,3")).toThrowError(
-    "negative numbers not allowed: -1, -2"
-  );
-});
