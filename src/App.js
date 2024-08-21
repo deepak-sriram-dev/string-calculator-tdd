e6dbc472d939b5099a8da2695240fb34860c2822
deepak-sriram <deepaksriramdev@gmail.com>
2024-08-21 17:28:47 +0530

    added calculator component with text field

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import StringCalculator from "./components/StringCalculator";
+
+function App() {
+  return (
+    <div className="App" style={{ height: "100%" }}>
+      <StringCalculator />
+    </div>
+  );
+}
+
+export default App;
